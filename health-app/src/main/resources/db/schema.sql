fd3f07eb8101c63d2a2647195d5f62c43415f7bf
angie-W-auemail <awang@algomau.ca>
2025-03-31 18:26:48 -0400

    added table activities

--- health-app/src/main/resources/db/schema.sql
+++ health-app/src/main/resources/db/schema.sql
@@ -12,10 +12,20 @@ CREATE TABLE
         role VARCHAR(10) NOT NULL, -- 'admin', 'trainer', 'trainee'
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (trainerID) REFERENCES users (userID) ON DELETE SET NULL
+        FOREIGN KEY (trainerID) REFERENCES users (user_id) ON DELETE SET NULL
     );
 CREATE TABLE
-    IF NOT EXISTS users (
+    IF NOT EXISTS activities (
         user_id INTEGER,
+        activity VARCHAR(20) NOT NULL, --"water", "food", "excercise"
+        calorie INTEGER, -- amount of calorie
+        waterintake INTEGER, -- grams of waterintake
+        trainee_weight DECIMAL(5, 2), --weight taken after activity, will also need to update in users
+        duration INTEGER, -- exercise duration (minutes)
+        input_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (user_id, input_datetime),
+        FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE SET NULL
+    );
+
 
 
