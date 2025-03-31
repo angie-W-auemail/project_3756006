c095a684b030b1547948f3ff7862511e959aaf45
angie-W-auemail <awang@algomau.ca>
2025-03-31 17:27:02 -0400

    trainer ui, trainers options clients and profile

---
+++ health-app/src/main/java/com/health/system/healthsystem/Controllers/Employee/TrainerController.java
@@ -0,0 +1,29 @@
+
+package com.health.system.healthsystem.Controllers.Employee;
+
+import com.health.system.healthsystem.Models.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TrainerController implements Initializable {
+    public BorderPane client_parent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Model.getInstance().getViewFactory().getTrainerSelectedMenuItem().addListener((observableValue, oldValue, newValue) -> {
+            switch(newValue) {
+                case PROFILE -> {
+                    Model.getInstance().getViewFactory().setProfileView(null);
+                    client_parent.setCenter(Model.getInstance().getViewFactory().getProfileView());
+                }
+                case CLIENTS -> {
+                    Model.getInstance().getViewFactory().setClientsView(null);
+                    client_parent.setCenter(Model.getInstance().getViewFactory().getClientsView());
+                }
+            }
+        });
+    }
+}
